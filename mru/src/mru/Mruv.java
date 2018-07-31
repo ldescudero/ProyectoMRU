8051b996f8503d8fdfb7db6362a7ef261e8c3110
ldescudero <38506158+ldescudero@users.noreply.github.com>
2018-07-31 12:31:34 -0500

    Add files via upload
    m mkmkmm

---
+++ mru/src/mru/Mruv.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mru;
+
+/**
+ *
+ * @author Mela
+ */
+public class Mruv {
+
+    public Mruv() {
+    }
+    
+    public double calcularVelocidadFinal(double vi,double ac,double tiempo){
+        return vi+(ac*tiempo);
+    }
+    
+    public double calcularDistancia(double vi,double vf,double tiempo){
+        return ((vi+vf)/2)*tiempo;
+    }
+    public double calcularTiempo(double vi,double vf,double aceleracion){
+        return (vf-vi)/aceleracion;
+    }
+    
+    public double calcularAceleracion(double vi,double vf,double tiempo){
+         return (vf-vi)/tiempo;
+    }
+}
