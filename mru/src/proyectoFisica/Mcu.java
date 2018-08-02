5978402874b72a3fd522bea4aca93c2fb718ead6
Melany Palacios <mjpc_10@hotmail.com>
2018-08-02 11:09:50 -0500

    no message

---
+++ mru/src/proyectoFisica/Mcu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoFisica;
+
+/**
+ *
+ * @author PC
+ */
+public class Mcu {
+     public double calcularVelocidadAngular(double angulo,double tiempo){
+        return angulo/tiempo;
+    }
+    
+    public double calcularAngulo(double tiempo,double velocidadAngular){
+        return velocidadAngular*tiempo;
+    }
+    public double calcularTiempo(double angulo,double velocidadAngular){
+        return angulo/velocidadAngular;
+    }
+    public double gradosARadianes(double grados){
+        return (grados*Math.PI)/180;
+    }
+}
---
+++ mru/src/proyectoFisica/Mru.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoFisica;
+
+/**
+ *
+ * @author Mela
+ */
+public class Mru {
+     public double calcularVelocidad(double distancia,double tiempo){
+        return distancia/tiempo;
+    }
+    
+    public double calcularDistancia(double tiempo,double velocidad){
+        return velocidad*tiempo;
+    }
+    public double calcularTiempo(double distancia,double velocidad){
+        return distancia/velocidad;
+    }
+}
